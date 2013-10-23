3f538691c6194e130e57ee03c3ee73f851a8be84
Joe Yuhas <joe.yuhas@gmail.com>
2013-10-23 14:59:08 -0700

    changed check time to 15 minutes because beautiful soup will have to reconnect to the status site

--- stream_check.py
+++ stream_check.py
@@ -9,10 +9,11 @@ soup = BeautifulSoup(urllib2.urlopen('http://stream.kpsu.org:8080/').read())
 
 while True:
 	try:
+		soup = BeautifulSoup(urllib2.urlopen('http://stream.kpsu.org:8080/').read())
 		j = soup.findAll('table')[1] # if the mount point is active this will be fine
 	except IndexError:
 		subprocess.call(['./test.sh']) # other wise the mount point has failed
 		print "stream failed, restarting"
 		time.sleep(900)
-	print "stream is fine"	
-	time.sleep(2)
+	print "stream is fine"
+	time.sleep(900) # check every 15 minutes	
