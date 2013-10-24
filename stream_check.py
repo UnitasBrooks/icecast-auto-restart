8b245af1883874ae111f4e8ffe878ab5c7752ed9
Joe Yuhas <joe.yuhas@gmail.com>
2013-10-23 22:16:41 -0700

    added my test script, needs the first line or python will throw an error, also needs chmod +x

--- stream_check.py
+++ stream_check.py
@@ -5,15 +5,14 @@ from bs4 import BeautifulSoup
 # or if you're using BeautifulSoup4:
 # from bs4 import BeautifulSoup
 #'http://www.diliak.net:8000'
-soup = BeautifulSoup(urllib2.urlopen('http://stream.kpsu.org:8080/').read())
 
 while True:
 	try:
 		soup = BeautifulSoup(urllib2.urlopen('http://stream.kpsu.org:8080/').read())
-		j = soup.findAll('table')[1] # if the mount point is active this will be fine
+		j = soup.findAll('table')[2] # if the mount point is active this will be fine
 	except IndexError:
-		subprocess.call(['./test.sh']) # other wise the mount point has failed
-		print "stream failed, restarting"
+		print "stream failed, restarting" # otherwise the mount point has faileds
+		subprocess.call(['./test.sh']) 
 		time.sleep(900)
 	print "stream is fine"
-	time.sleep(900) # check every 15 minutes	
+	time.sleep(120) # check every 2 minutes	
