d7010eb024eed282ed274cc482bb60d138db27ba
Joe Yuhas <joe.yuhas@gmail.com>
2013-10-24 13:37:14 -0700

    added a try catch if the internet drops and split the while loop into functions

--- stream_check.py
+++ stream_check.py
@@ -5,17 +5,40 @@ import subprocess
 from bs4 import BeautifulSoup
 # Diliak is a icecast server that at the time I wrote this always failed
 # http://www.diliak.net:8000/
+
+def connection_error():
+	print "Internet dropped"
+	text_file = open("stream_failure_log.txt","a")
+	text_file.write("Internet dropped at: ")
+	text_file.write(str(datetime.now()))
+	text_file.write("\n")
+	text_file.close()
+
+def stream_dropped():
+	print "stream failed, restarting" # otherwise the mount point has failed
+	subprocess.call(['./test.sh']) 
+	text_file = open("stream_failure_log.txt","a")
+	text_file.write("stream failed at: ")
+	text_file.write(str(datetime.now()))
+	text_file.write("\n")
+	text_file.close()
+	time.sleep(900)
+
 while True:
-	soup = BeautifulSoup(urllib2.urlopen('http://stream.kpsu.org:8080').read())
+	# check internet connection
+	try:
+		soup = BeautifulSoup(urllib2.urlopen('http://stream.kpsu.org:8080').read())
+	except urllib2.URLError:
+		connection_error()
+		break
+
 	tables = soup.findAll('table') # there should be three tables present, otherwise the stream is down
+
 	if len(tables) != 3:
-		print "stream failed, restarting" # otherwise the mount point has failed
-		subprocess.call(['./test.sh']) 
-		text_file = open("stream_failure_log.txt","a")
-		text_file.write("stream failed at: ")
-		text_file.write(str(datetime.now()))
-		text_file.write("\n")
-		text_file.close()
-		time.sleep(900)
+		stream_dropped()
+		
 	print "stream is fine"
-	time.sleep(120) # check every 2 minutes	
+	print str(datetime.now())
+	time.sleep(15) # check every 15 seconds	
+
+
