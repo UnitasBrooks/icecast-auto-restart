9a09c713c76658740bad42e0de8b0b3cc3b2d11b
Joe Yuhas <joe.yuhas@gmail.com>
2013-10-24 13:21:07 -0700

    took out some comments, put in some better ones

--- stream_check.py
+++ stream_check.py
@@ -3,14 +3,12 @@ import time
 from datetime import datetime
 import subprocess
 from bs4 import BeautifulSoup
-# or if you're using BeautifulSoup4:
-# from bs4 import BeautifulSoup
-#'
-
+# Diliak is a icecast server that at the time I wrote this always failed
+# http://www.diliak.net:8000/
 while True:
-	soup = BeautifulSoup(urllib2.urlopen('http://www.diliak.net:8000/').read())
-	j = soup.findAll('table')
-	if len(j) != 3:
+	soup = BeautifulSoup(urllib2.urlopen('http://stream.kpsu.org:8080').read())
+	tables = soup.findAll('table') # there should be three tables present, otherwise the stream is down
+	if len(tables) != 3:
 		print "stream failed, restarting" # otherwise the mount point has failed
 		subprocess.call(['./test.sh']) 
 		text_file = open("stream_failure_log.txt","a")
